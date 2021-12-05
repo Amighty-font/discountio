df58a1ad41e8bdf0b94db6132b460b4678f6f522
Amighty-font <timothyfang9@gmail.com>
2021-12-04 19:20:16 -0800

    Updated ui

---
+++ discountio/src/Components/NavBar.js
@@ -0,0 +1,67 @@
+
+
+import React, { Component } from 'react'
+import { Container, Navbar, NavDropdown, Form, FormControl, Button, Nav } from 'react-bootstrap'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+
+import Home from './Home';
+import Contact from './Account';
+import About from './List'
+
+export default class NavbarComp extends Component {
+    render() {
+        return (
+
+            <Router>
+        <div>
+        <Navbar bg="light" expand="lg">
+        <Container fluid>
+            <Navbar.Brand href="#">Discountio</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+            <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+            >
+                <Nav.Link as={Link} to={"/home"}>Home</Nav.Link>
+                <Nav.Link as={Link} to={"/account"}>Account</Nav.Link>
+                <Nav.Link as={Link} to={"/list"}>List</Nav.Link>
+                {/* <Nav.Link href="#" disabled>
+                hellooo lets go
+                </Nav.Link> */}
+            </Nav>
+            <Form className="d-flex">
+                <FormControl
+                type="search"
+                placeholder="Item Name"
+                className="me-2"
+                aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+            </Form>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+        </ div>
+        <div>
+            <Route path="/home">
+                <Home />
+            </Route>
+            <Route path="/list">
+                <List />
+            </Route>
+            <Route path="/account">
+                <Account />
+            </Route>
+        </div>
+            </Router>
+        
+        )
+    }
+}
