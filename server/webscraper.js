6027d8b90714cfc7d7f336dec55fa5f6fcff151a
Eric Sue <ericsue@students.cs.ubc.ca>
2021-12-04 20:05:07 -0800

    added for loop which notifies user

--- server/webscraper.js
+++ server/webscraper.js
@@ -162,37 +162,23 @@ async function scrapeProductPrice(url) {
 
 // var list_of_products = [normal, sold_out_product, not_sold_out_anymore, notify_sale, list_of_products];
 
+// list of products to be added by tim using the website
+list_of_products = []
 
-// for (let i = 0; i < list_of_products.length - 1; i++) {
-//     var link = list_of_products[i].getLink();
-//     var curr_price = list_of_products[i].getPrice;
+for (let i = 0; i < list_of_products.length - 1; i++) {
+    var link = list_of_products[i].getLink();
+    var curr_price = list_of_products[i].getPrice;
 
-//     if (list_of_products[i].getSoldOut()) {
-//         if(!scrapeProductSoldOut(link)) {
-//             list_of_products[i].setSoldOut(0);
-//             list_of_products[i].setPrice(scrapeProductPrice(link));
-//             list_of_products[i].setRestocked(1);
-//         }
-//     } else {
-//         var updated_price = scrapeProductPrice(link);
-//         if (updated_price == -1) {
-//             list_of_products[i].setPrice(0);
-//             list_of_products[i].setSoldOut(1);
-//         } else if (updated_price < curr_price) {
-//             list_of_products[i].setPrice(updated_price);
-//             list_of_products[i].setOnSale(1);
-//         }
-//     }
-
-// }
-
-
-// normal
-// scrapeProduct('https://www.amazon.ca/Amazon-Ethernet-Adapter-Fire-Devices/dp/B01LXP5TXI/ref=p13n_ds_purchase_sim_1p_dp_desktop_1/141-2361534-6361661?pd_rd_w=NHonj&pf_rd_p=b320fab9-b132-4bb6-a1bd-e1a1eb3bd47e&pf_rd_r=8T9AWGCAZXHAWYJJQFWV&pd_rd_r=4be29f59-7e1e-4ed7-9deb-eb4b0dcc1b84&pd_rd_wg=EWiKv&pd_rd_i=B01LXP5TXI&psc=1');
-
-// deal
-// scrapeProduct('https://www.amazon.ca/fire-tv-stick-with-3rd-gen-alexa-voice-remote/dp/B08C1TR9X6?ref_=Oct_d_obs_d_667823011&pd_rd_w=qrVzL&pf_rd_p=886d55ac-d17c-4912-a5af-948a0d840eec&pf_rd_r=AZJFJEQENB51SZDE6BDX&pd_rd_r=0dec4ba1-a834-40ec-a247-deca17d97058&pd_rd_wg=a4hGC&pd_rd_i=B08C1TR9X6');
-// scrapeProduct('https://www.amazon.ca/Rechargeable-Operated-Electric-Motorcycle-S66B/dp/B08GSTR85G?ref_=Oct_DLandingS_D_352714a7_60&smid=AQPJ60TYAP2UO&th=1&psc=1')
-
-// unavailable
-// scrapeProduct('https://www.amazon.ca/Made-Canada-level-Black-Mask/dp/B0924YB4L5/ref=sr_1_131?keywords=face+mask&qid=1638659654&refinements=p_n_availability%3A12035748011&rnid=12035746011&sr=8-131')
+    if (list_of_products[i].getSoldOut()) {
+        if (!scrapeProductSoldOut) {
+            // text user that item is not sold out and update the product information
+        }
+    } else {
+        var updated_price = scrapeProductPrice(link);
+        if (updated_price == -1) {
+            // sold out (idk if we should notify user)
+        } else if (updated_price < curr_price) {
+            // notify user about discount
+        }
+    }
+}
